cf891b9b9c86181778f2269ed7a950b8f4e5e05d
llucien <llucien@194.57.88.71>
2016-12-09 16:07:45 +0100

    first version

---
+++ femto.HACCA/src/femto/HACCA/HACCA.java
@@ -0,0 +1,10 @@
+package femto.HACCA;
+
+import msi.gama.precompiler.GamlAnnotations.skill;
+import msi.gama.precompiler.IConcept;
+
+@skill(name="HACCA", concept={IConcept.SKILL})
+
+public class HACCA {
+
+}
